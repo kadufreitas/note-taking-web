29c2ecc7964f2c45a9666acff1c6358a00be34c5
kadufreitas <kadufreitas95@gmail.com>
2019-11-28 01:55:04 -0300

    Updates

---
+++ precache-manifest.78fc59c1203be64d967013da35875ee2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a4d02a2b38877c00c34635da861020",
+    "url": "/note-taking-web/index.html"
+  },
+  {
+    "revision": "5e05a1675f0f1b625fc5",
+    "url": "/note-taking-web/static/js/2.006d8d36.chunk.js"
+  },
+  {
+    "revision": "d2bbb222b61e7de50d90",
+    "url": "/note-taking-web/static/js/main.8642cd19.chunk.js"
+  },
+  {
+    "revision": "8025395b551f1195e559",
+    "url": "/note-taking-web/static/js/runtime-main.ca2140f9.js"
+  }
+]);
